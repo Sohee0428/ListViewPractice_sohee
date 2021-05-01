3c9c21242e66b58ad54190079a997dac860f7deb
Sohee <sohee3480@gmail.com>
2021-05-01 14:25:07 +0900

    어댑터 변수 추가, 리스트뷰의 어댑터로써 연결

--- app/src/main/java/com/example/listviewpractice_sohee/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice_sohee/MainActivity.kt
@@ -2,27 +2,32 @@ package com.example.listviewpractice_sohee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewpractice_sohee.adapters.StudentAdapter
 import com.example.listviewpractice_sohee.datas.Student
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 
     val mStudentList = ArrayList<Student>()
 
+    lateinit var mAdapter: StudentAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mStudentList.add( Student("권소희", 1997) )
-        mStudentList.add( Student("정상훈", 1994) )
-        mStudentList.add( Student("김지영", 1997) )
-        mStudentList.add( Student("권재훈", 1999) )
-        mStudentList.add( Student("김도아", 2020) )
-        mStudentList.add( Student("전영주", 1997) )
+        mStudentList.add(Student("권소희", 1997))
+        mStudentList.add(Student("정상훈", 1994))
+        mStudentList.add(Student("김지영", 1997))
+        mStudentList.add(Student("권재훈", 1999))
+        mStudentList.add(Student("김도아", 2020))
+        mStudentList.add(Student("전영주", 1997))
 
+        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
 
-
+        studentListVIew.adapter = mAdapter
     }
 
 
