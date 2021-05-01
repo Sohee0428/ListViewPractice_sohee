b34d4aae729cb936307e63b9fec1017707d1ff35
Sohee <sohee3480@gmail.com>
2021-05-01 14:14:03 +0900

    학생 목록 ArrayList 생성, 실제 목록 추가

--- app/src/main/java/com/example/listviewpractice_sohee/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice_sohee/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.listviewpractice_sohee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewpractice_sohee.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+
+    val mStudentList = ArrayList<Student>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add( Student("권소희", 1997) )
+        mStudentList.add( Student("정상훈", 1994) )
+        mStudentList.add( Student("김지영", 1997) )
+        mStudentList.add( Student("권재훈", 1999) )
+        mStudentList.add( Student("김도아", 2020) )
+        mStudentList.add( Student("전영주", 1997) )
+
+
+
     }
+
+
 }
