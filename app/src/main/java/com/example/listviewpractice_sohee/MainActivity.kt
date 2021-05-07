f53ef6ea8b31ebf0aa4a36c40815aa1a9dab23e5
Sohee <sohee3480@gmail.com>
2021-05-07 19:37:31 +0900

    for문 작성

--- app/src/main/java/com/example/listviewpractice_sohee/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice_sohee/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.listviewpractice_sohee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.example.listviewpractice_sohee.adapters.StudentAdapter
 import com.example.listviewpractice_sohee.datas.Student
@@ -19,12 +20,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         mStudentList.add(Student("권소희", 1997))
         mStudentList.add(Student("정상훈", 1994))
         mStudentList.add(Student("김지영", 1997))
         mStudentList.add(Student("권재훈", 1999))
         mStudentList.add(Student("김도아", 2020))
         mStudentList.add(Student("전영주", 1997))
+
+        for (std in mStudentList) {
+            Log.d("학생 이름", std.name)
+        }
+
+
+        for ( i in 0 until 5) {
+            Log.d("숫자", i.toString())
+        }
 
         mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
 
